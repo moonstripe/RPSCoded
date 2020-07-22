f918c32c618b8bf947ae1bee0f36400c42cb0aa8
Kojin Glick <kojinglick@gmail.com>
2020-07-21 18:12:47 -0700

    logic in place and tested

--- 23-RPS-Coded/javascript.js
+++ 23-RPS-Coded/javascript.js
@@ -4,36 +4,50 @@ var gameStates = ["w", "l", "t"];
 // Inputs: player, computer choices. Outputs: game results
 
 function shoot(ai, player) {
-    if (ai === ) {
-        
-    } else {
-        
-    }
+    if (ai === player) {
+        return "t";
+
+        // player loses
+    } else if (ai === "rock" && player === "scissors") {
+        return "l";
+    } else if (ai === "scissors" && player === "paper") {
+        return "l";
+    } else if (ai === "paper" && player === "rock") {
+        return "l";
+        // player wins
+    } else if (ai === "scissors" && player === "rock") {
+        return "w";
+    } else if (ai === "rock" && player === "paper") {
+        return "w";
+    } else if (ai === "paper" && player === "scissors") {
+        return "w";
+    } 
 }
 
+// Iterates while adding games
 
+let games = 0;
+let wins = 0;
+let losses = 0;
 
 while (games < 10) {
     var playerChoice = prompt("Do you choose 'rock', 'paper', or 'scissors'?");
     const aiChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
+    alert("ai chose "+aiChoice);
 
-    var gameResult = shoot(aiChoice, playerChoice)
+    var gameResult = shoot(aiChoice, playerChoice);
 
     if (gameResult === "t") {
-        alert("You tied with the Computer, try again")
-    } else if () {
-
+        alert("You tied with the Computer, try again");
+    } else if (gameResult === "w") {
+        alert("You won against the Computer!");
+        wins++;
+        games++;
+    } else if (gameResult === "l") {
+        alert("You got your ass beat...");
+        losses++;
+        games++;
     }
 }
 
-for (let index = 0; index < 10; index++) {
-    var playerChoice = prompt("Do you choose 'rock', 'paper', or 'scissors'?")
-    const aiChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
-    if (shoot(aiChoice, playerChoice) === t) {
-        
-    } else {
-        
-    
-    }
-     fdsafdfshaskfhdadsff
-}
+alert("You won "+wins+" games out of 10!");
