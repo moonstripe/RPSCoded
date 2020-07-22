5e1d8caff9990c01acf74ca47bb4a43abc5f70e4
Kojin Glick <kojinglick@gmail.com>
2020-07-21 17:37:21 -0700

    RPS init

---
+++ 23-RPS-Coded/javascript.js
@@ -0,0 +1,39 @@
+var rpsChoices = ["rock", "paper", "scissors"];
+var gameStates = ["w", "l", "t"];
+
+// Inputs: player, computer choices. Outputs: game results
+
+function shoot(ai, player) {
+    if (ai === ) {
+        
+    } else {
+        
+    }
+}
+
+
+
+while (games < 10) {
+    var playerChoice = prompt("Do you choose 'rock', 'paper', or 'scissors'?");
+    const aiChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
+
+    var gameResult = shoot(aiChoice, playerChoice)
+
+    if (gameResult === "t") {
+        alert("You tied with the Computer, try again")
+    } else if () {
+
+    }
+}
+
+for (let index = 0; index < 10; index++) {
+    var playerChoice = prompt("Do you choose 'rock', 'paper', or 'scissors'?")
+    const aiChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
+    if (shoot(aiChoice, playerChoice) === t) {
+        
+    } else {
+        
+    
+    }
+    
+}
